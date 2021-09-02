d8b338bdd19596f51bc07cac98824d4a409d2ec2
zhangyue44 <zy1617768774@163.com>
2021-09-03 00:14:09 +0800

    :construction: feat: 修改proxy中的路径与登录表单添加keyup事件

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -10,7 +10,7 @@
         <el-input v-model="loginForm.username"></el-input>
       </el-form-item>
       <el-form-item label="密码" prop="password">
-        <el-input v-model="loginForm.password" show-password></el-input>
+        <el-input v-model="loginForm.password" show-password @keyup.enter.native="submitLoginForm('loginForm')"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="danger" @click="submitLoginForm('loginForm')">用户登录</el-button>
@@ -43,6 +43,10 @@ export default {
         ]
       }
     };
+  },
+  //  只能在mounted生命周期中， created中报错 原因自己想笨蛋
+  mounted() {
+    this.resetForm('loginForm'); // 刷新就会自动重置表单
   },
   methods: {
     submitLoginForm(formName) {
--- webpackconfig/webpack.dev.js
+++ webpackconfig/webpack.dev.js
@@ -9,7 +9,8 @@ module.exports = {
     open: true,
     proxy: {
       "/pan": {
-        target: "http://192.168.239.86:8002",
+        target: "http://192.168.31.119:8002",
+        // target: "http://localhost:8002", // 自己笔记本的ip总是经常变，所以也可以直接用 localhost
         pathRewrite: {
           "^/pan": ""
         },
